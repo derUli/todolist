e4a72fa443c51ba1ba20575d6038b854cd7110fd
Ulrich Schmidt <admin@deruli.de>
2017-10-01 14:05:52 +0200

    translation

--- src/content/modules/todolist/lang/en.php
+++ src/content/modules/todolist/lang/en.php
@@ -3,4 +3,4 @@ idefine("TRANSLATION_TODOLIST", "To-Do List");
 idefine("TRANSLATION_UP", "Up");
 idefine("TRANSLATION_DOWN", "Down");
 idefine("TRANSLATION_TODO_TASKS", "Todo Tasks");
-idefine("TRANSLATION_OPEN_TODOLIST","Open ToDo List");
+idefine("TRANSLATION_OPEN_TODOLIST", "Open To-Do List");
