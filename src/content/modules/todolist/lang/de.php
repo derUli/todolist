35fe96b0f92edc256538042a09c9ff4aedfb4e33
Ulrich Schmidt <admin@deruli.de>
2017-10-01 13:25:23 +0200

    Offene Aufgaben werden auf dem Dashboard angezeigt

--- src/content/modules/todolist/lang/de.php
+++ src/content/modules/todolist/lang/de.php
@@ -1,4 +1,6 @@
 <?php
-idefine ( "TRANSLATION_TODOLIST", "Aufgabenliste" );	
-idefine ( "TRANSLATION_UP", "Hoch" );
-idefine ( "TRANSLATION_DOWN", "Runter" );
+idefine("TRANSLATION_TODOLIST", "Aufgabenliste");
+idefine("TRANSLATION_UP", "Hoch");
+idefine("TRANSLATION_DOWN", "Runter");
+idefine("TRANSLATION_TODO_TASKS", "Zu erledigende Aufgaben");
+idefine("TRANSLATION_OPEN_TODOLIST", "Aufgabenliste öffnen");
