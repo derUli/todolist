97a1e24518425c344c6936491672757ff3247556
Ulrich Schmidt <admin@deruli.de>
2018-12-11 07:49:17 +0100

    fix conflict in foreign key name

--- src/content/modules/todolist/sql/up/002.sql
+++ src/content/modules/todolist/sql/up/002.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `{prefix}todolist_items`
-  ADD CONSTRAINT fk_user_id 
+  ADD CONSTRAINT fk_todolist_user_id 
   FOREIGN KEY (user_id) 
   REFERENCES `{prefix}users`(id) 
   ON DELETE CASCADE;
